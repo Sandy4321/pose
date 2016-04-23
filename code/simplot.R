7de6cfedf6f113be3b5e667c41587f356f387997
Matt Taddy <taddy@chicagobooth.edu>
2016-04-22 22:38:16 -0500

    new sim material

--- code/simplot.R
+++ code/simplot.R
@@ -75,8 +75,10 @@ printR2tab <- function(nobs, design, support, fname=""){
 \\begin{table}
 \\vspace{-.2cm}
 \\footnotesize
-\\caption{ {\\bf  %d observations, %s design with %s covariates.}
-  Average out-of-sample $R^2$, reported as  \\%% worse than the Oracle 
+\\caption{ 
+	{\\bf  Predictive $\\boldsymbol{R^2}$ for %d observations, 
+	%s design with %s covariates.}
+  Reported as  \\%% worse than the Oracle 
   -- MLE fit on the %s covariates -- 
   across 1000 samples.}
 \\begin{center}
@@ -102,7 +104,7 @@ $\\mathrm{sd}(\\boldsymbol{\\eta})/\\sigma$ & {\\sf d} & $\\rho$
 		cat("\\hline\\rule{0pt}{3ex}\n", file=fname, append=TRUE)
 		for(decay in c(10,50,100,200)){
 			for(rho in c(0,0.5,.9))
-				printR2line("r2",nobs, design, support, rho,s2n,decay, fname=fname)
+				printR2line(nobs, design, support, rho,s2n,decay, fname=fname)
 			if(decay!=200) cat("\\rule{0pt}{3ex}\n", file=fname, append=TRUE)
 		}
 	}
@@ -112,65 +114,69 @@ $\\mathrm{sd}(\\boldsymbol{\\eta})/\\sigma$ & {\\sf d} & $\\rho$
 \n", file=fname, append=TRUE)
 }
 
-
-printdetail <- function(means, rho, s2n, decay, cpline, bg=NULL){
+printdetail <- function(fname, means, rho, s2n, decay, cpline, bg=NULL){
 	for(s in c("CV.1se","CV.min","AICc","AIC","BIC")){
 		l <- paste(c(s, sprintf("%s",  
 			means[paste(c("gl0","gl1","gl10","mrg"),s,sep=".")])),
 		collapse=" & ")
+		if(!is.null(bg)) amps <- "& & &"
+		else amps <- "&"
 		if(s=="CV.1se") 
-			cat(l,sprintf("& %s & & & \\\\\n", means["snet1se"]))
+			cat(l,sprintf("& %s %s \\\\\n", means["snet1se"], amps), file=fname, append=TRUE)
 		if(s=="CV.min"){
 			if(!is.null(bg))
 			 cat(l,
-				sprintf("& %s & %s & & $\\mr{sd}(\\bm{\\mu})/\\sigma=%g$ \\\\\n", 
-					means["snetmin"], bg["CVerr"], s2n))
+				sprintf("& %s & %s & & $\\mathrm{sd}(\\mathbf{\\mu})/\\sigma=%g$ \\\\\n", 
+					means["snetmin"], bg["CVerr"], s2n), file=fname, append=TRUE)
 			else
 			 cat(l,
-				sprintf("& %s & & &  $\\mr{sd}(\\bm{\\mu})/\\sigma=%g$ \\\\\n", 
-					means["snetmin"], s2n))
+				sprintf("& %s %s  $\\mathrm{sd}(\\mathbf{\\mu})/\\sigma=%g$ \\\\\n", 
+					 means["snetmin"], amps, s2n), file=fname, append=TRUE)
 		}
 		if(s=="AICc"){
 		 	if(!is.null(bg))
-		 		cat(l, sprintf("& & & %s &  $\\rho=%g$ \\\\\n", bg["ICerr"], rho))
+		 		cat(l, sprintf("& & & %s &  $\\rho=%g$ \\\\\n", bg["ICerr"], rho), file=fname, append=TRUE)
 		 	else
-		 		cat(l, sprintf("& & & &  $\\rho=%g$ \\\\\n", rho))
+		 		cat(l, sprintf("& %s $\\rho=%g$ \\\\\n", amps, rho), file=fname, append=TRUE)
 		}
 		if(s=="AIC") cat(l,
-			sprintf("& & & &  %s \\\\\n", cpline))
-		if(s=="BIC") cat(l,"& & & &  \\\\\n \\hline \n")
+			sprintf("& %s  %s \\\\\n", amps, cpline), file=fname, append=TRUE)
+		if(s=="BIC") cat(l,sprintf("& %s  \\\\\n \\hline \n",amps), file=fname, append=TRUE)
 	}
 	invisible()
 }
 
 
-printMSE <- function(nobs, design, support, decay){
+printMSE <- function(fname="", nobs, design, support, decay){
 	preamble <- sprintf(
 "\n
-\\begin{table}[p]\\vspace{-.5cm}
-\\caption[l]{ Predictive MSE, for %d observations, %s design, %s covariates, and  \\textsf{d}=%d.}
+\\begin{table}\\vspace{-.5cm}
+\\caption[l]{ { \\bf Predictive MSE for n=%d, %s design, 
+%s covariates, and  decay  %d}.}
 \\vspace{-.5cm}
-\\small\\setstretch{1}
+\\footnotesize\\setstretch{1}
 \\begin{center}
 \\begin{tabular}{l*{7}{c}|r}
- & lasso & GL $\\gamma=1$ & GL $\\gamma=10$ & AL & MCP  & ICbest & CVbest  \\\\
-\\cline{1-7}
+ & lasso & GL $\\gamma=1$ & GL $\\gamma=10$ & AL & MCP  & CVbest & ICbest  \\\\
+\\cline{1-9}
 ", nobs, design, support, decay)
-	cat(preamble)
+	cat(preamble, file=fname, append=TRUE)
 	for(s2n in c(2,1,0.5)){
 		for(rho in c(0,0.5,0.9)){
-			bg <- round(colMeans(getit("bestgamma", nobs, design, support, rho, s2n, decay))[c(2,4)],2)
+			bg <- round(colMeans(getit("bestgamma", 
+				nobs, design, support, rho, s2n, decay))[c(2,4)],2)
 			bgs <- sprintf("%.2f", bg)
 			names(bgs) <- names(bg)
 			means <- round(colMeans(getit("mse", 
-				nobs=nobs, design=design, support=support, rho=rho, s2n=s2n, decay),na.rm=TRUE),2)
+				nobs=nobs, design=design, support=support, rho=rho, s2n=s2n, decay),
+				na.rm=TRUE),2)
 			ismin <- which(means==min(means[-1]))
 			means[-ismin] <- sprintf("%.2f",means[-ismin])
 			means[ismin] <- sprintf("{\\bf %s}",means[ismin])
-			cpline = sprintf("\\multirow{2}{*}{$Oracle: $ = %s}", means["Oracle"])
+			cpline = sprintf("\\multirow{2}{*}{$Oracle: $ %s}", means["Oracle"])
 			bgmin <- which(bg==min(means[-1]))
 			bgs[bgmin] <- sprintf("{\\bf %s}",bgs[bgmin])
-			printdetail(means, rho, s2n, decay, cpline, bgs)
+			printdetail(fname, means, rho, s2n, decay, cpline, bgs)
 		}
 	}
 	cat(
@@ -178,24 +184,112 @@ printMSE <- function(nobs, design, support, decay){
 \\end{center}
 \\vspace{-1cm}
 \\end{table}
-\n")
+\n", file=fname, append=TRUE)
 }
 
 
-printsens <- function(decay){
+
+printEMSE <- function(fname="", nobs, design, support, decay){
+	preamble <- sprintf(
+"\n
+\\begin{table}\\vspace{-.5cm}
+\\caption[l]{ { \\bf Estimation MSE for n=%d, %s design, 
+%s covariates, and  decay  %d}.}
+\\vspace{-.5cm}
+\\footnotesize\\setstretch{1}
+\\begin{center}
+\\begin{tabular}{l*{5}{c}|r}
+& lasso & GL $\\gamma=1$ & GL $\\gamma=10$ & marginal AL & sparsenet MCP  & \\\\
+ \\cline{1-7}
+", nobs, design, support, decay)
+	cat(preamble, file=fname, append=TRUE)
 	for(s2n in c(2,1,0.5)){
 		for(rho in c(0,0.5,0.9)){
-			fdr <- round(colMeans(getit("fdr", rho=rho, s2n=s2n, decay),na.rm=TRUE),1)
-			sens <- round(colMeans(getit("sens", rho=rho, s2n=s2n, decay),na.rm=TRUE),1)
+			means <- round(colMeans(getit("mse", 
+				nobs=nobs, design=design, support=support, rho=rho, s2n=s2n, decay),
+				na.rm=TRUE),2)
+			ismin <- which(means==min(means[-1]))
+			means[-ismin] <- sprintf("%.2f",means[-ismin])
+			means[ismin] <- sprintf("{\\bf %s}",means[ismin])
+			cpline = sprintf("\\multirow{2}{*}{$Oracle: $ %s}", means["Oracle"])
+			printdetail(fname, means, rho, s2n, decay, cpline)
+		}
+	}
+	cat(
+"\\end{tabular}
+\\end{center}
+\\vspace{-1cm}
+\\end{table}
+\n", file=fname, append=TRUE)
+}
+
+
+
+printS <- function(fname="", nobs, design, support, decay){
+	preamble <- sprintf(
+"\n
+\\begin{table}\\vspace{-.5cm}
+\\caption[l]{ { \\bf Nonzero coefficients at n=%d, %s design, 
+%s covariates, and  decay  %d}.}
+\\vspace{-.5cm}
+\\footnotesize\\setstretch{1}
+\\begin{center}
+\\begin{tabular}{l*{5}{c}|r}
+& lasso & GL $\\gamma=1$ & GL $\\gamma=10$ & marginal AL & sparsenet MCP  & \\\\
+ \\cline{1-7}
+", nobs, design, support, decay)
+	cat(preamble, file=fname, append=TRUE)
+	for(s2n in c(2,1,0.5)){
+		for(rho in c(0,0.5,0.9)){
+			means <- round(colMeans(getit("s", 
+				nobs=nobs, design=design, support=support, rho=rho, s2n=s2n, decay),
+				na.rm=TRUE),2)
+			cpline = sprintf("\\multirow{2}{*}{$Oracle: $ %s}", means["Oracle"])
+			printdetail(fname, means, rho, s2n, decay, cpline)
+		}
+	}
+	cat(
+"\\end{tabular}
+\\end{center}
+\\vspace{-1cm}
+\\end{table}
+\n", file=fname, append=TRUE)
+}
+
+
+printsens <- function(fname="", nobs, design, support, decay){
+	preamble <- sprintf(
+"
+\\begin{table}\\vspace{-.5cm}
+\\caption[l]{ {\\it }
+{ \\bf FDR $\\boldsymbol{\\mid}$ Sensitivity for n=%d, %s design, %s covariates, and  decay  %d}.}
+\\vspace{-.5cm}
+\\footnotesize\\setstretch{1}
+\\begin{center}
+\\begin{tabular}{l*{5}{c}|r}
+ & lasso & GL $\\gamma=1$ & GL $\\gamma=10$ & marginal AL & sparsenet MCP  & \\\\
+ \\cline{1-7}
+", nobs, design, support, decay)
+	cat(preamble, file=fname, append=TRUE)
+	for(s2n in c(2,1,0.5)){
+		for(rho in c(0,0.5,0.9)){
+			fdr <- round(colMeans(getit("fdr", nobs=nobs, design=design, support=support, rho=rho, s2n=s2n, decay),na.rm=TRUE),1)
+			sens <- round(colMeans(getit("sens", nobs=nobs, design=design, support=support, rho=rho, s2n=s2n, decay),na.rm=TRUE),1)
 			means <- sprintf( "%.02f $\\mid$ %.02f", fdr/100, sens/100)
 			names(means) <- names(fdr)
 			avg <- fdr+(100-sens)
-			sCp <- mean(getit("s",rho=rho,s2n=s2n, decay)[,1])
+			sCp <- mean(getit("s", nobs=nobs, design=design, support=support, rho=rho,s2n=s2n, decay)[,1])
 			#means[avg==min(avg[-1])] <- sprintf("{\\bf %s}",means[avg==min(avg[-1])])
-			cpline = sprintf("\\multirow{2}{*}{$\\bar{s}_{C_p}$ = %.01f}",sCp)
-			printit(means, rho, s2n, decay, cpline)
+			cpline = sprintf("\\multirow{2}{*}{$\\bar{s}_{Oracle}$ = %.01f}",sCp)
+			printdetail(fname, means, rho, s2n, decay, cpline)
 		}
 	}
+cat(
+"\\end{tabular}
+\\end{center}
+\\vspace{-1cm}
+\\end{table}
+\n", file=fname, append=TRUE)
 }
 
 
@@ -203,7 +297,7 @@ printsens <- function(decay){
 printsupp <- function(fname){
 	doctop <- 
 "\\documentclass[12pt]{article}
-\\usepackage{amssymb,amsmath,setspace,anysize,times,dsfont}
+\\usepackage{amssymb,amsmath,setspace,anysize,times,dsfont,multirow}
 \\usepackage{caption}
 \\captionsetup{
   font=small,
@@ -219,16 +313,67 @@ printsupp <- function(fname){
 \\setcounter{section}{6}
 \\setcounter{table}{2}
 \\setcounter{figure}{3}
-\\setstretch{1.3}\n\n"	
+\\setstretch{1.2}
 
-cat(doctop, file=fname)
+\\section{Simulation study: full results}\n\n"	
+
+cat("%%!TEX root = supplemental.tex\n\n", file=fname)
+
+print("summary")
+cat("\\subsection{Summary out-of-sample prediction results}\n\n", file=fname, append=TRUE)
 for(nobs in c(100,1000))
 	for(support in c("dense","sparse"))
 		for(design in c("binary","continuous")){
-			printtab(nobs,design,support,fname=fname)
+			printR2tab(nobs,design,support,fname=fname)
 			cat("\n\n",file=fname, append=TRUE)
 		}
+
+cat("\\clearpage\n\n", file=fname, append=TRUE)
+print("mse")
+cat("\\subsection{Detailed out-of-sample prediction results}\n\n", file=fname, append=TRUE)
+for(nobs in c(100,1000))
+	for(support in c("dense","sparse"))
+		for(design in c("binary","continuous"))
+			for(decay in c(10,50,100,200)){		
+				printMSE(fname=fname, nobs,design,support,decay)
+				cat("\n\n",file=fname, append=TRUE)
+		}
+
+cat("\\clearpage\n\n", file=fname, append=TRUE)
+print("estimation")
+cat("\\subsection{Estimation error relative to true parameters}\n\n", file=fname, append=TRUE)
+for(nobs in c(100,1000))
+	for(support in c("dense","sparse"))
+		for(design in c("binary","continuous"))
+			for(decay in c(10,50,100,200)){		
+				printEMSE(fname=fname, nobs,design,support,decay)
+				cat("\n\n",file=fname, append=TRUE)
+		}
+
+cat("\\clearpage\n\n", file=fname, append=TRUE)
+print("support")
+cat("\\subsection{Number of estimated nonzero coefficients}\n\n", file=fname, append=TRUE)
+for(nobs in c(100,1000))
+	for(support in c("dense","sparse"))
+		for(design in c("binary","continuous"))
+			for(decay in c(10,50,100,200)){		
+				printS(fname=fname, nobs,design,support,decay)
+				cat("\n\n",file=fname, append=TRUE)
+		}
+
+print("sensitivity")
+cat("\\clearpage\n\n", file=fname, append=TRUE)
+cat("\\subsection{Sensitivity and false discovery relative to true parameters
+ (sparse covariates) or the $\\boldsymbol{C_p}$ Oracle (dense covariates). }\n\n", file=fname, append=TRUE)
+for(nobs in c(100,1000))
+	for(support in c("dense","sparse"))
+		for(design in c("binary","continuous"))
+			for(decay in c(10,50,100,200)){		
+				printsens(fname=fname, nobs,design,support,decay)
+				cat("\n\n",file=fname, append=TRUE)
+		}
+
 cat("\\end{document}\n",file=fname, append=TRUE)
 }
-
 printsupp("paper/simulations.tex")
+#printsens(fname="", 1000,"binary","sparse",100)
